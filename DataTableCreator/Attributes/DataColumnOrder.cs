8a7c4f13492f2de5f8dc96cd28bba463c6f834c2
Bharat Kumar Bellamkonda <pavinan@users.noreply.github.com>
2017-11-17 19:42:55 +0530

    Add project files.

---
+++ DataTableCreator/Attributes/DataColumnOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTableCreator.Attributes
+{
+    public class DataColumnOrderAttribute : Attribute
+    {
+        public int Order;
+        public DataColumnOrderAttribute(int order)
+        {
+            Order = order;
+        }
+    }
+}
---
+++ DataTableCreator/DataTableCreator.cs
@@ -0,0 +1,59 @@
+﻿using DataTableCreator.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTableCreator
+{
+    public class DataTableCreator<T>
+    {
+
+        private readonly DataTable _underlyingDataTable = new DataTable();
+
+        private List<KeyValuePair<string, PropertyDetails>> _propertyCache = new List<KeyValuePair<string, PropertyDetails>>();
+
+        private DataTableCreator()
+        {
+
+        }
+
+        public static DataTableCreator<T> GetCreator()
+        {
+            var creator = new DataTableCreator<T>();
+
+            var dataType = typeof(T);
+
+            foreach (var propertyItem in dataType.GetProperties())
+            {
+                var details = new PropertyDetails
+                {
+                    PropertyInfo = propertyItem
+                };
+
+                var propName = propertyItem.Name;
+
+                var nameAttr = propertyItem.GetCustomAttributes<DataColumnNameAttribute>();
+
+                if(nameAttr.Any())
+                {
+                    details.DataColumnName = nameAttr.First();
+                }
+
+                creator._propertyCache.Add(new KeyValuePair<string, PropertyDetails>(propName, details));
+            }
+
+            return creator;
+        }
+
+        private class PropertyDetails
+        {
+            public PropertyInfo PropertyInfo { get; set; }
+            public DataColumnNameAttribute DataColumnName { get; set; }
+        }
+
+    }
+}
