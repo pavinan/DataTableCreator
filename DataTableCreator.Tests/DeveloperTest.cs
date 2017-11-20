3583f50629d31793eeb4142e8feb5ea0bc4d18b9
Bharat Kumar Bellamkonda <pavinan@users.noreply.github.com>
2017-11-20 16:53:34 +0530

    Added rows add.

--- DataTableCreator.Tests/DeveloperTest.cs
+++ DataTableCreator.Tests/DeveloperTest.cs
@@ -10,8 +10,26 @@ namespace DataTableCreator.Tests
         public void Run()
         {
             var tableCreator = DataTableCreator<Student>.GetCreator();
+            
+            var student = new Student
+            {
+                Id = "1",
+                Address = "asdsad",
+                RNo= 1,
+                FirstName = "Bharat",
+                LastName = "Ku",
+                DOB = DateTime.Now.AddYears(-23).Date
+            };
 
+            tableCreator
+                .Add(student)
+                .Add(student)
+                .Add(student)
+                .Add(student)
+                .Add(student)
+                .Add(student);
 
+            var dt = tableCreator.GetDataTable();
         }
     }
 
--- DataTableCreator/DataTableCreator.cs
+++ DataTableCreator/DataTableCreator.cs
@@ -26,6 +26,36 @@ namespace DataTableCreator
             return _underlyingDataTable;
         }
 
+        public DataTableCreator<T> Add(T data)
+        {
+            var row = _underlyingDataTable.NewRow();
+
+            foreach (var propertyItem in _propertyCache)
+            {
+                var columnValue = propertyItem.Value.PropertyInfo.GetValue(data);
+
+                row[propertyItem.Value.ColumnName] = columnValue;
+
+            }
+
+            _underlyingDataTable.Rows.Add(row);
+
+            return this;
+        }
+
+        private void CreateDataTable()
+        {
+            foreach (var item in _propertyCache)
+            {
+                var columnName = item.Value.DataColumnName?.Name ?? item.Value.PropertyInfo.Name;
+
+                item.Value.ColumnName = columnName;
+
+                _underlyingDataTable.Columns.Add(columnName, item.Value.PropertyInfo.PropertyType);
+
+            }
+        }
+
         public static DataTableCreator<T> GetCreator()
         {
             var creator = new DataTableCreator<T>();
@@ -34,6 +64,9 @@ namespace DataTableCreator
 
             foreach (var propertyItem in dataType.GetProperties())
             {
+                if (!IsSimple(propertyItem.PropertyType))
+                    continue;
+
                 var details = new PropertyDetails
                 {
                     PropertyInfo = propertyItem
@@ -43,7 +76,7 @@ namespace DataTableCreator
 
                 var nameAttr = propertyItem.GetCustomAttributes<DataColumnNameAttribute>();
 
-                if(nameAttr.Any())
+                if (nameAttr.Any())
                 {
                     details.DataColumnName = nameAttr.First();
                 }
@@ -51,13 +84,31 @@ namespace DataTableCreator
                 creator._propertyCache.Add(new KeyValuePair<string, PropertyDetails>(propName, details));
             }
 
+            creator.CreateDataTable();
+
             return creator;
+        }
+
+        static bool IsSimple(Type type)
+        {
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                // nullable type, check if the nested type is simple.
+                return IsSimple(type.GetGenericArguments()[0]);
+            }
+            return type.IsPrimitive
+              || type.IsEnum
+              || type.Equals(typeof(string))
+              || type.Equals(typeof(decimal))
+              || type.Equals(typeof(DateTime))
+              || type.Equals(typeof(DateTimeOffset));
         }
 
         private class PropertyDetails
         {
             public PropertyInfo PropertyInfo { get; set; }
             public DataColumnNameAttribute DataColumnName { get; set; }
+            public string ColumnName { get; set; }
             public int Order { get; set; }
         }
 
