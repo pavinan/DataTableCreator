0823330d64aad6a7c2aa42b9c21f40b6ee10f86c
Bharat Kumar Bellamkonda <pavinan@users.noreply.github.com>
2017-11-20 11:41:11 +0530

    Added unit test.

---
+++ DataTableCreator.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataTableCreator.Tests
+{
+    [TestClass]
+    public class DeveloperTest
+    {
+        [TestMethod]
+        public void Run()
+        {
+
+        }
+    }
+}
